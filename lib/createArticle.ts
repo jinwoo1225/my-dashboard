2033d62bab40a4f5df351c85957f4f53407c3a2c
Hong Jinwoo (Carter) <jinwoo17962@icloud.com>
2022-08-21 01:18:53 +0900

    refactor library file convention (#3)

--- lib/createArticle.ts
+++ lib/createArticle.ts
@@ -1,4 +1,4 @@
-export const create_article = async (data: any) => {
+export const createArticle = async (data: any) => {
     let response = await fetch("http://localhost:3000/api/articles/",
         {
             method: "POST",
